79f0816df3e9f3363985f043ee24dc1f207dcb25
giaonx <giao5fs@gmail.com>
2024-10-05 07:01:30 +0700

    update

--- api1/obj/Release/net8.0/api1.AssemblyInfo.cs
+++ api1/obj/Release/net8.0/api1.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("api1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b10c1c4c90487f5c502f49e2d962c289ab71404")]
 [assembly: System.Reflection.AssemblyProductAttribute("api1")]
