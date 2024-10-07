e7320d8c64ac0dc5fb0e18a8fbb4ef8d663b3bc6
giaonx <giao5fs@gmail.com>
2024-10-08 04:05:37 +0700

    update webapp

--- webapp/battlehungerVueWebAppTest/src/App.vue
+++ webapp/battlehungerVueWebAppTest/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <NavBar />
-    <h1>Welcome to the Microsoft Authentication Library For Vue Quickstart</h1>
+    <h1>Welcome to summoner rift</h1>
     <router-view />
   </div>
 </template>
