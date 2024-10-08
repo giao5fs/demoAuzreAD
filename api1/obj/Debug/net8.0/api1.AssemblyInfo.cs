1f045cefcafd19bce638c7736bfa04a731d42793
giaonx <giao5fs@gmail.com>
2024-10-08 12:28:53 +0700

    update

--- api1/obj/Debug/net8.0/api1.AssemblyInfo.cs
+++ api1/obj/Debug/net8.0/api1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d189d62a08a3e0e1991c1a4eb649c6642a76ddea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1f73b099f0ddfc2f30c42a929a63b103b94fedd")]
 [assembly: System.Reflection.AssemblyProductAttribute("api1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
