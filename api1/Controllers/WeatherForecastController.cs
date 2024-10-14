b7757ee0ac0bb0f88ae4dc948dbf03015d99c8bc
giaonx <giao5fs@gmail.com>
2024-10-14 18:52:03 +0700

    update

--- api1/Controllers/WeatherForecastController.cs
+++ api1/Controllers/WeatherForecastController.cs
@@ -26,6 +26,7 @@ public class WeatherForecastController : ControllerBase
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
